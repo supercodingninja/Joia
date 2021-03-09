6b0b32965e9f4b53bbb638fd0d5551f68802110d
Andrew Olson <frrrkin@gmail.com>
2021-03-08 23:51:32 -0800

    configured server build and dev scripts

---
+++ server/src/server.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import path from "path";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app:express.Application = express();
+const port = process.env.PORT || 3001;
+
+let clientstaticpath = "";
+
+if(process.env.NODE_ENV == "production") {
+    clientstaticpath = "clientbuild"; // Verify this works once the client build works.
+}
+else {
+    console.log(`env.NODE_ENV is ${process.env.NODE_ENV}.  Express won't host the client site.`);
+}
+
+console.log(`clientstaticpath is set to '${clientstaticpath}'`)
+
+if(clientstaticpath != "") {
+    app.use(express.static(clientstaticpath));
+}
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
