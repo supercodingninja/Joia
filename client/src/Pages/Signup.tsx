648f54ac0133af8c0b4e21d771834eaa1f0c8325
SCN <supercodingninja@outlook.com>
2021-03-16 06:55:24 -0700

    Login page completed, and Signup simplified.

--- client/src/Pages/Signup.tsx
+++ client/src/Pages/Signup.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { Header } from "./header"; // Speak to the team about this. //
 import CSS from 'csstype';
 
 const Signup = () => {
@@ -22,31 +21,13 @@ const Signup = () => {
     )
 }
 
-// export default Signup
+export default Signup
 
-// // Ref. https://stackoverflow.com/questions/41628247/how-to-include-css-file-in-tsx-typescript //
-// export class Home extends React.Component<{}, {}> {
-//     render() {
-//         const Signup: { scss: React.ReactNode; }.scss = `
-//         .Signup-page {
-//             background-color: #E5E5E5;
-//         }
-//         `
-//         return (
-//             <div>
-//                 <style>
-//                     {Signup.scss}
-//                 </style>
-
-//                 <div id="body" className="Signup-page">
-//                     <Header />
-//                 </div>
-//             </div>  
-//         );
-//     }
-// }
-
-const h1Styles: CSS.Properties = {
-  backgroundColor: '#E5E5E5',
-  backgroundSize: 'cover'
+const bodyStyles: CSS.Properties = {
+    backgroundColor: '#E5E5E5',
+    backgroundSize: 'cover'
 };
+  
+export function Heading({ title } : { title: string} ) {
+    return <body style={bodyStyles}>{title}</body>;
+}
