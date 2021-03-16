86fec51f198799a0d4954b1eac31854b58e301f3
Rattanak Leng <rleng@live.com>
2021-03-15 20:47:55 -0700

    upload structure

---
+++ client/src/components/HomePageMain/HomePageMain.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import {Input, TextArea, FormBtn} from "../Form/Form";
+
+
+const HomePageMain = () => {
+    return (
+ 
+        <div className="m-0 bg-light pt-5">
+            <h2 className="text-center">Location</h2>      
+
+        </div>  
+
+    )
+}
+
+export default HomePageMain
