b1bac669d4a4c2a2629208be4aca5385836a169c
con0fav <connor.favero@gmail.com>
2021-03-15 17:52:43 -0700

    pseudocode

--- client/src/components/shared/Footer/Footer.tsx
+++ client/src/components/shared/Footer/Footer.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import './footer.css';
 
 const Footer = () => {
     return (
         <div>
-            <h1>footer</h1>
+            <h1>Joia</h1>
         </div>
     )
 }
+
+//elements from left to right:
+//home
+//contact us
+//team members
+//PICTURE LOGO IN CENTER
+//about
+//search
+//sell
+
+//bottom elements underneath nav
+//dummy social media icons (link to #)
+//joia copyright info?
 
 export default Footer
