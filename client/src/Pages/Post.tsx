ed777b458b5cdff6a55c7ba733d5503b6ad9439f
SCN <supercodingninja@outlook.com>
2021-03-16 07:10:24 -0700

    Post.tsx page created

--- client/src/Pages/Post.tsx
+++ client/src/Pages/Post.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import CSS from 'csstype';
 
-const Post = () => {
+const Profile = () => {
     return (
-        <div>
-            <h1>This is Post</h1>
-        </div>
+        <>
+            <nav></nav>
+
+            <div>
+                <h2>Profile</h2>
+            </div>
+
+            <form></form>
+
+            <button>Update Profile</button>
+            
+            <button>Sell Your Artwork</button>
+
+            <footer></footer>
+        </>
     )
 }
 
-export default Post
+export default Profile
+
+const bodyStyles: CSS.Properties = {
+    backgroundColor: '#E5E5E5',
+    backgroundSize: 'cover'
+};
+  
+export function Heading({ title } : { title: string} ) {
+    return <body style={bodyStyles}>{title}</body>;
+}
