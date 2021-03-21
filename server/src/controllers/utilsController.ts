7fadafcdd6cbb738cd798cac7aa029683bfcd094
Andrew Olson <frrrkin@gmail.com>
2021-03-20 17:38:46 -0700

    updated the seed route to seed with valid user data

--- server/src/controllers/utilsController.ts
+++ server/src/controllers/utilsController.ts
@@ -5,8 +5,20 @@ export async function seed(req: express.Request, res: express.Response)
 {
     try {
         await db.User.deleteMany({});
-        await db.User.create({firstName:"firstName1", lastName:"lastName1"})
-        await db.User.create({firstName:"firstName2", lastName:"lastName2"})
+        await db.User.create({
+            name:"Name1",
+            email:"email1@gmail.com",
+            phone:"12345",
+            location:"Seattle",
+            password:"secret"
+        });
+        await db.User.create({
+            name:"Name2",
+            email:"email2@gmail.com",
+            phone:"56789",
+            location:"Seattle",
+            password:"secret"
+        });
     } catch(err) {
         res.status(422).json(err);
     }
