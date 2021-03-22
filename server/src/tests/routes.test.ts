84f65f9c5c9d4b1c4c87a1ee1262af732f8a2c16
Andrew Olson <frrrkin@gmail.com>
2021-03-21 18:35:34 -0700

    made dire warnings about test timing and improved the user post-route test

--- server/src/tests/routes.test.ts
+++ server/src/tests/routes.test.ts
@@ -11,9 +11,6 @@ describe("a test", () => {
     });
 });
 
-let app: express.Application = configureApp(false);
-let server: http.Server;
-
 function createTimeoutPromise(milliseconds: number) {
     let ret: Promise<Number> = new Promise(function(resolve, reject) {
 
@@ -24,59 +21,73 @@ function createTimeoutPromise(milliseconds: number) {
 
 describe("an app test", () => {
 
+    let app: express.Application = configureApp(false);
+    let server: http.Server;
+
+    
     beforeEach(() => {
-        server = app.listen(3000, () => console.log("listening on port 3000"))
+        server = app.listen(3000);
     });
 
-    afterEach(async () => {
-        server.close();
-        await createTimeoutPromise(500);
+    // frankly, accepting done is dangerous.
+    // It BETTER be called when done and not forgotten
+    // if it is accepted as a param
+    // NEVER accept done as a parameter and neglect to call it
+    // In fact, accepting done as a parameter AT ALL is dangerous because
+    // if code is changed so that done is no longer called, you'll have
+    // a hanging test and a huge mystery on your hands.  This error has
+    // killed way too much of my time - don't let it kill yours.
+    afterEach(async (done) => {
+        server.close(() => {
+            // here, it is used because server.close is not async-style
+            // it is callback-style (boooo)
+            done();
+        });        
     });
 
     beforeAll(async () => {
-        // TODO switch to a test database
+        // TODO switch to a use mocking for the backend
+        // ferrealz database IO is no good for testing
         await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/test", { useNewUrlParser: true });
         try {
             await db.User.collection.drop();
         } catch(e) {
-            console.log("drop failed, threw: ", e);
+            console.log("drop User.collection failed, threw: ", e);
         };
     });
 
     afterAll(async () => {
-        await mongoose.disconnect();
+        await mongoose.disconnect();        
     });
 
-    it("crufty no-mocks real-test-database route to create a new user", async (done) => {
-        const whocares = await request(app).get('/')
+    it("crufty no-mocks real-test-database route to create a new user", async () => {
 
+        // this is sort of nasty and generally should be avoided when it can be
+        // however, database IO can be slow and this test runs the entire gamut
+        // all the way through the server routes to the database so the scope
+        // is very broad and the IO is heavy.
+        // To put it more briefly, this is NOT a *unit* test.
+        // It is more of a functional one
+        jest.setTimeout(10000);
+               
         const res = await request(app)
         .post("/api/users")
         .send({
-            name:"Name3",
+            name:"ATestAddedMe",
             email:"email3@gmail.com",
             phone:"12345",
             location:"Seattle",
             password:"333333"
-        })
+        });       
         
         expect(res.status).toEqual(200);
         
-        // of course the following is a hack.
-        // I need to turn it into a promise
-        // then expect() to verify it.
-        // As it stands, I am not verifying my data -
-        // only that the server returned a 200 status code
-        db.User.count((err, count) => {
-            console.log("count cb got count = ", count)
-            expect(count).toBe(1);
-            done();  
-            // mongoose does not deal in promises and I *should*
-            // somehow promisify this.
-            // it isn't as easy as using the promisify package because
-            // TypeScript hates it for not doing the @types thing.
-            // I suggest no one follows my lead in doing this until I figure
-            // out a way to avoid running expect inside the callback.
-        });     
+        let allUsers = await db.User.find({});
+        
+        expect(allUsers.length).toBe(1);
+        
+        let justTheOneUser = allUsers[0];
+
+        expect(justTheOneUser.name).toBe("ATestAddedMe");        
     });
 });
