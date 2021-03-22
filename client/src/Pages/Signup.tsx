40238ac29390b390bce4969e7e64b8760e139b84
SCN <supercodingninja@outlook.com>
2021-03-22 14:04:37 -0700

    attempted logic approach to signup

--- client/src/Pages/Signup.tsx
+++ client/src/Pages/Signup.tsx
@@ -1,4 +1,6 @@
 // import React from 'react';
+import { Signup } from 'react-stormpath';
+
 import './Signup.css';
 // import {container } from '../LayoutComponents/Grid/Grid'
 import {
@@ -52,6 +54,47 @@ const Signup = () => {
             </div>
         </>
     )
-}
+};
+
+
+Signup.post('/me', bodyParser.json(), stormpath.loginRequired, function (req, res) {
+    function writeError(message) {
+      res.status(400);
+      res.json({ message: message, status: 400 });
+      res.end();
+    }
+  
+    function saveAccount () {
+      req.user.givenName = req.body.givenName;
+      req.user.surname = req.body.surname;
+      req.user.email = req.body.email;
+  
+      req.user.save(function (err) {
+        if (err) {
+          return writeError(err.userMessage || err.message);
+        }
+        res.end();
+      });
+    }
+  
+    if (req.body.password) {
+      var application = req.app.get('stormpathApplication');
+  
+      application.authenticateAccount({
+        username: req.user.username,
+        password: req.body.existingPassword
+      }, function (err) {
+        if (err) {
+          return writeError('The existing password that you entered was incorrect.');
+        }
+  
+        req.user.password = req.body.password;
+  
+        saveAccount();
+      });
+    } else {
+      saveAccount();
+    }
+  });
 
 export default Signup;
