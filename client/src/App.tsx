f70b653df927b843c23ec09703df7fb19d310b4d
SCN <supercodingninja@outlook.com>
2021-03-23 15:54:10 -0700

    Working out function kinks on local machine (not seen in this push); I need to ensure that anyone viewing this branch can see that the updates from the main are included in my branch.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,53 +22,66 @@ import { useAuthTokenStore}  from "./utils/auth";
 // Including the useAuthTokenStore. //
 // Including the useAuthTokenStore hook will allow your app to reauthenticate already logged in users if they refresh the page, or leave and return to the application. //
 // The best place for this is inside your primary App component. //
-class App extends React.Component {
-  state: { currentUser: any; };
-
+// Ref. https://reactjs.org/docs/react-without-es6.html; and https://github.com/supercodingninja/mern-jwt-auth-setup#prerequisites forked from ac524/mern-jwt-auth-setup //
+function App () {
   // Use the hook to reauthenticate stored tokens.
-  useAuthTokenStore();
+  useAuthTokenStore()
 
   /** Rest of your App component code here */
-  // Reference https://jasonwatmore.com/post/2019/04/06/react-jwt-authentication-tutorial-example; and https://www.alibabacloud.com/blog/how-to-implement-authentication-in-reactjs-using-jwt_595820 //
-  constructor(props) {
-    super(props);
+  // Reference https://jasonwatmore.com/post/2019/04/06/react-jwt-authentication-tutorial-example; and https://www.alibabacloud.com/blog/how-to-implement-authentication-in-reactjs-using-jwt_595820 //  
+  return (
+    <Router>
+      <Navbar />
 
-    this.state = {
-      currentUser: null
-    };
-  }
+      <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/post" component={ProductPost} />
+          <Route exact path="/account" component={Profile} />
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/Signup" component={Signup} />
+      </Switch>
 
-  componentDidMount() {
-    authenticationService.currentUser.subscribe(x => this.setState({ currentUser: x }));
-  }
-  setState(arg0: { currentUser: any; }) {
-    throw new Error("Method not implemented.");
-  }
+      <Footer />      
+    </Router>
+  );
 
-  logout() {
-    authenticationService.logout();
-    history.push('/login');
-  }
+  function LoginForm() {
 
-  render() {
-    const { currentUser } = this.state;
-    
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    // Get the helper login function from the `useLogin` hook.
+    const login = useLogin();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+      const email = emailRef.current.value;
+      const password = passwordRef.current.value;
+
+      try {
+
+          await login({ email, password });
+
+          // User has been successfully logged in and added to state. Perform any additional actions you need here such as redirecting to a new page.
+
+      } catch(err) {
+
+            // Handle error responses from the API
+            if( err.response && err.response.data ) console.log(err.response.data);
+
+      }
+    }
+
     return (
-      <Router>
-        <Navbar />
-
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/post" component={ProductPost} />
-            <Route exact path="/account" component={Profile} />
-            <Route exact path="/Login" component={Login} />
-            <Route exact path="/Signup" component={Signup} />
-        </Switch>
-
-        <Footer />      
-      </Router>
-    );
+        <form onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <input type="text" ref={emailRef} placeholder="Your email" /><br />
+            <input type="password" ref={passwordRef} placeholder="Your password" /><br />
+            <button>Submit</button>
+        </form>
+    )
   }
 };
 
