db591d5e2971e2f9f2fd502364f22c972da1660f
SCN <supercodingninja@outlook.com>
2021-03-23 04:59:45 -0700

    Added a new variable for JWT_SECRET; and

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,8 @@ import * as dotenv from "dotenv";
 import mongoose from "mongoose";
 import configureApp from "./app";
 
+let JWT_SECRET = "A random string, which is used to help generate unique keys. This can be anything you want, quotes, a short passage from a book, random letters."
+
 // this is the server's main entrypoint from the beginning
 // It is responsible for setting up the entire app environment
 // as it was launched, including mongo setup and calling .listen
