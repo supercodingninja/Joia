36fdb6dde823a665a100e44d36788f0498e2a892
Frederick Thomas <supercodingninja@outlook.com>
2021-03-26 16:55:28 -0700

    Cleaner forms

--- client/src/Pages/Signup.tsx
+++ client/src/Pages/Signup.tsx
@@ -1,15 +1,20 @@
 import React, {useState} from 'react';
 import {Container} from '../LayoutComponents/Grid/Grid'
 import {Input, FormBtnOutline, FormBtn} from '../components/shared/Form/Form'
-import {useAuthTokenStore, useIsAuthenticated, useAuthenticatedUser, useLogin, useLogout} from '../utils/auth';
+// import {useAuthTokenStore, useIsAuthenticated, useAuthenticatedUser, useLogin, useLogout} from '../utils/auth';
+import {useLogin} from '../utils/auth';
 import API from '../utils/api';
 import { useHistory } from 'react-router-dom'
 
 const Signup = () => {
+    // HOW DO WE USE THESE. //
+    // const authTokenStore = useAuthTokenStore();
+    // const isAuthenticated = useIsAuthenticated();
+    // const authenticatedUser = useAuthenticatedUser();
+    // const  logout = useLogout();
 
     // Get the helper login function from the `useLogin` hook. //
     const login = useLogin();
-
     const history = useHistory();
 
     const [firstName, setFirstName] = useState('');
@@ -42,14 +47,9 @@ const Signup = () => {
             // User has been successfully registered, now log them in with the same information. //
             await login({email, password});
 
-            
-            // USE MODAL, HERE: User has been successfully registered, logged in and added to state. Additional actions will be performed, here, to redirect Profile.tsx, while transferring the data, also.  Use history.push('./account');; here. //
-
-            
-
         } catch(err) {
 
-             // Handle error responses from the API. This will include
+             // Handle error responses from the API. //
              if(err.response && err.response.data) {
                  console.log(err.response.data);
 
@@ -66,35 +66,58 @@ const Signup = () => {
 
     return (
         <Container>
-                <h1 className='text-center py-5'>
-                    Ready to sign up to sell your artwork?
+                <h1 className='text-center py-2'>
+                    Discover <i><b>Joia</b></i>
                 </h1>
     
-                <p className='text-center mt-2 mb-5'>
+                <p className='text-center mt-2 mb-2'>
                     <i>Joia</i> is your jewel eCommerce website for local art, where you can find your gem for your Jewel or Jim!
                 </p>
     
-                <div className='container px-3 mb-5 pb-5'>
-                    <form onSubmit={handleFormSubmit} name='signupForm' className='border border-warning py-5 mx-3 px-5'>
+                <div className='container px-1 mb-1 pb-5'>
+                    <form onSubmit={handleFormSubmit} name='signupForm' className='py-2 mx-1 px-4'>
                         
-                        <h5>First Name:</h5>
-                        <Input type='text' onChange={(e)=>setFirstName(e.target.value)} name='firstName'/>
+                        <div className='row align-items-center justify-content-center'>
+                            <h5>First Name:</h5>
+                            &nbsp;&nbsp;&nbsp;
+                            <Input type='text' onChange={(e)=>setFirstName(e.target.value)} name='firstName'/>
+                        </div>
+
     
-                        <h5>Last Name:</h5>
-                        <Input type='text' onChange={(e)=>setLastName(e.target.value)} name='lastName'/>
-    
-                        <h5>Email:</h5>
-                        {emailAlreadyTaken && (<h1 style={{color:'red'}}>Try a different email.  That one is taken</h1>)}
-                        <Input type='text' onChange={(e)=>setEmail(e.target.value)} name='email'/>
-    
-                        <h5>Phone:</h5>
-                        <Input type='text' onChange={(e)=>setPhone(e.target.value)} name='phone'/>
-    
-                        <h5>Password:</h5>
-                        <Input type='text' onChange={(e)=>setPassword(e.target.value)} name='password'/>
-                        
-                        <h5>Location:</h5>
-                        <Input type='text' onChange={(e)=>setLocation(e.target.value)} name='location'/>
+                        <div className='row align-items-center justify-content-center'>
+                            <h5>Last Name:</h5>
+                            &nbsp;&nbsp;&nbsp;
+                            <Input type='text' onChange={(e)=>setLastName(e.target.value)} name='lastName'/>
+                        </div>
+
+
+                        <div className='row align-items-center justify-content-center'>
+                            <h5>Email:</h5>
+                            {emailAlreadyTaken && (<h1 style={{color:'red'}}>Try a different email.  That one is taken</h1>)}
+                            &nbsp;&nbsp;&nbsp;
+                            <Input type='text' onChange={(e)=>setEmail(e.target.value)} name='email'/>
+                        </div>
+
+
+                        <div className='row align-items-center justify-content-center'>
+                            <h5>Phone:</h5>
+                            &nbsp;&nbsp;&nbsp;
+                            <Input type='text' onChange={(e)=>setPhone(e.target.value)} name='phone'/>
+                        </div>
+
+
+                        <div className='row align-items-center justify-content-center'>
+                            <h5>Password:</h5>
+                            &nbsp;&nbsp;&nbsp;
+                            <Input type='text' onChange={(e)=>setPassword(e.target.value)} name='password'/>
+                        </div>
+                            
+
+                        <div className='row align-items-center justify-content-center'>   
+                            <h5>Location:</h5>
+                            &nbsp;&nbsp;&nbsp;
+                            <Input type='text' onChange={(e)=>setLocation(e.target.value)} name='location'/>
+                        </div>
 
                         <FormBtnOutline onClick={handleFormClear}>
                             Cancel
