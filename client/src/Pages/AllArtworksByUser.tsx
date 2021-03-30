09d21a3948b50a5d7b673e366290e4644bd20843
Rattanak Leng <rleng@live.com>
2021-03-29 18:26:08 -0700

    commit to push

--- client/src/Pages/AllArtworksByUser.tsx
+++ client/src/Pages/AllArtworksByUser.tsx
@@ -1,30 +1,41 @@
 import React, { useEffect, useState } from 'react'
 import ArtItem from '../components/ArtItem'
 import api from '../utils/api'
-import {useAuthenticatedUser} from '../utils/auth';
+import { useAuthenticatedUser } from '../utils/auth'
 
 export default function AllArtworksByUser() {
+  const [allArtByUser, setAllArtByUser] = useState<any[]>([])
 
-    const [allArtByUser, setAllArtByUser] = useState<any[]>([]);
+  const user = useAuthenticatedUser()
 
-    const user = useAuthenticatedUser();
+  useEffect(() => {
+    const asyncBody = async () => {
+      let allArt: any = await api.getAllArtByUser(user._id)
+      setAllArtByUser(allArt.data)
+    }
 
-    useEffect(() => {
-        const asyncBody = async () => {
-            let allArt:any = await api.getAllArtByUser(user._id);
-            setAllArtByUser(allArt.data);
-        }
+    asyncBody().then(() => console.log('done getting user art'))
+  }, [])
 
-        asyncBody().then(() => console.log("done getting user art"));
-
-    }, []);
-
-    return (
-        <>
-            <h2>all art by the current user.</h2>
-            <ul>
-            {allArtByUser.map((anArt,i) => <ArtItem showcontactinfo={false} userid={""} name={anArt.name} imagePath={anArt.imagePath} description={anArt.description} category={anArt.category} size={anArt.size} price={anArt.price}></ArtItem>)}
-            </ul>
-        </>
-    )
+  return (
+    <div className="bg-light">
+      <div className="container pb-5">
+        <h2 className="text-center my-5">Your Artworks</h2>
+        <ul>
+          {allArtByUser.map((anArt, i) => (
+            <ArtItem
+              showcontactinfo={false}
+              userid={''}
+              name={anArt.name}
+              imagePath={anArt.imagePath}
+              description={anArt.description}
+              category={anArt.category}
+              size={anArt.size}
+              price={anArt.price}
+            ></ArtItem>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
