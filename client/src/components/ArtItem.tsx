185245084808646952b05416d127e111f0ea638e
Rattanak Leng <rleng@live.com>
2021-03-29 17:21:04 -0700

    fix artItem component

--- client/src/components/ArtItem.tsx
+++ client/src/components/ArtItem.tsx
@@ -1,45 +1,70 @@
-import { useEffect, useState } from "react"
-import api from '../utils/api'
-
-export default function ArtItem({showcontactinfo, userid, name, imagePath, description, category, size, price}: {showcontactinfo:boolean, userid:string, name:string, imagePath:string, description:string, category: string, size: string, price: string}) {
-    // I think my git was flipping out because this component was GONE on a 
-    // fresh checkout from main and I know I added it in a prior change so
-    // I re-added it by grabbing it from somewhere else on my hard drive.
-    // But then once, I did so which allowed me to run the project, running
-    // "git status" didn't notice the file was changed or added.  It seems
-    // as if git is messing up how to track it.
-    // So I did an explicit add of this using the fiull path and finally it got
-    // the idea that it needed to add it.  Stupid thing.  So here's crossing
-    // my fingers that this forces it to do whatever needs doing to track it.
-
-    const [authorUser, setAuthorUser] = useState({})
-
-    useEffect(() => {
-        async function asyncStuff() {
-            let userWhoSubmittedTheArt = await api.getUserById(userid)
-            setAuthorUser(userWhoSubmittedTheArt.data);
-        }
-
-        asyncStuff().then(() => {
-            console.log("user has been retrieved for the art item.");
-        });
-    },[]);
-
-    return (
-
-        <li>
-        <img src={imagePath} alt={name}></img>
-        <br/>
-        title: {name} <br/>
-        {showcontactinfo && (
-            <div>
-        author: {authorUser.name} <br/>
-        location: {authorUser.location} <br/>
-        email: {authorUser.email} <br/>
-        phone: {authorUser.phone} <br/>
-        </div>
-        )}
-        </li>
-        
-    )
+interface IArtItemProps {
+  name: string
+  description: string
+  category: string
+  size: string
+  price: string | number
+  location: string
+  artistName: string
+  contact: string
+  imagePath: string
 }
+
+const ArtItem = (props: IArtItemProps) => {
+  const {
+    name,
+    description,
+    category,
+    size,
+    price,
+    location,
+    artistName,
+    contact,
+    imagePath,
+  } = props
+
+  return (
+    <div className="card mb-3 border border-warning">
+      <div className="row no-gutters">
+        <div className="col-md-5">
+          <img
+            className="img-fluid bg-dark"
+            src={imagePath}
+            alt="Artwork thumnail"
+          />
+        </div>
+        <div className="col-md-7 border border-warning border-top-0 border-right-0 border-bottom-0">
+          <div className="card-body">
+            <h4 className="card-title text-uppercase">{name}</h4>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <span className="font-weight-bold mr-3">Category:</span>
+              {category}
+            </p>
+            <p className="card-text">
+              <span className="font-weight-bold mr-3">Size:</span>
+              {size}
+            </p>
+            <p className="card-text">
+              <span className="font-weight-bold mr-3">Price:</span>
+              {price}
+            </p>
+            <p className="card-text">
+              <span className="font-weight-bold mr-3">Location:</span>
+              {location}
+            </p>
+            <p className="card-text">
+              <span className="font-weight-bold mr-3">Artist:</span>
+              {artistName}
+            </p>
+            <p className="btn btn-outline-warning ">
+              <a href={`mailto:${contact}`}>Contact Artist</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ArtItem
